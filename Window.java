caefbf23ec8439e04267f08a4a70a12ac15621a4
Skye Smith <mvsmith@uvm.edu>
2022-12-04 21:51:46 -0500

    Add header for nextImg function

--- Window.java
+++ Window.java
@@ -1,16 +1,21 @@
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.util.ArrayList;
 
 public class Window extends JFrame {
     private JLabel img = new JLabel();
 
-    public Window() {
-        img.setIcon(new ImageIcon("back.jpg"));
+    public Window(String filename) {
+        img.setIcon(new ImageIcon(filename));
 
         setUndecorated(true);
         
         add(img);
+    }
+
+    public String nextImg(String current, boolean aState, ArrayList<String> seqQueue) {
+        return "";
     }
 
     public void changeImage(String filename) {
